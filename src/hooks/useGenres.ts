e7a511f13cbe62afbf9ab057b118b5233ac90552
Hoomaan <id@hoomaan.dev>
2024-03-15 18:29:35 +0400

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+    // An array of dependencies and the effect hook. Without this, we constantly send a request to our backend.
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
