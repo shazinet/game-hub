4e162bd8286c9b2f59f7e8a13bc9d6c80b11b570
Hoomaan <id@hoomaan.dev>
2024-05-06 07:08:27 +0400

    Update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '4a0759f479e24aa1bd70e4e9b8893a1f'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c722f5288eca483ca9fdaccd1ab0be55",
+  },
+});
